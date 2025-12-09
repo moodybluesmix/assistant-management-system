cad745e62d1b59eca83050cc5048fe31e50cd17e
melike <126975315+moodybluesmix@users.noreply.github.com>
2025-12-09 04:41:05 +0300

    Add files via upload

---
+++ ViewModels/RandevuYonetimViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AsistanNobetYonetimi.Models;
+
+namespace AsistanNobetYonetimi.ViewModels
+{
+	public class RandevuYonetimViewModel
+	{
+        
+        
+            public IEnumerable<Musaitlik> Musaitlikler { get; set; }
+            public IEnumerable<Randevu> Randevular { get; set; }
+        [Required]
+        public int MusaitlikID { get; set; }
+
+        [Required]
+        public string RandevuTarihi { get; set; }
+
+        [Required]
+        public string TimeSlot { get; set; }
+
+        [Required]
+        public string OgretimUyesi { get; set; }
+    }
+}
+
+    
+
+
